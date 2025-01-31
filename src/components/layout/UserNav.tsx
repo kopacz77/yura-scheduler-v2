02d7d52c10104a374a2cc22b08482d8822de4f85
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-01-30 23:24:53 -0500

    Update UserNav to use fallback avatar

--- src/components/layout/UserNav.tsx
+++ src/components/layout/UserNav.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -19,7 +19,6 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/yura.jpg" alt="Yura Min" />
             <AvatarFallback>YM</AvatarFallback>
           </Avatar>
         </Button>
