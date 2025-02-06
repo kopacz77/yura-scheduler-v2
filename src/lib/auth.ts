002993c4f6670c0ff2c6b38f3cb36c480a9ac3e4
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-05 22:20:55 -0500

    fix: update auth configuration to use @next-auth/prisma-adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { type NextAuthOptions } from 'next-auth';
 import { getServerSession } from 'next-auth/next';
 import CredentialsProvider from 'next-auth/providers/credentials';
