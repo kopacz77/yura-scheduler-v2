ca2aa52d8b339bc11f479ecd7cd6cd2dc44ff223
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-06 10:42:51 -0500

    fix: update type exports to include shared types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,37 +1,9 @@
 export * from './api';
-export * from './schema';
-export * from './context';
 export * from './components';
+export * from './context';
+export * from './shared';
 export * from './utils';
 
 // Re-export specific types that might be commonly used
-export type {
-  Student,
-  Rink,
-  TimeSlot,
-  AppState,
-  AppAction,
-} from './context';
-
-export type {
-  ApiResponse,
-  CreateLessonRequest,
-  UpdateLessonRequest,
-  CreatePaymentRequest,
-  VerifyPaymentRequest,
-} from './api';
-
-export type {
-  DataTableProps,
-  FormFieldProps,
-  ScheduleCalendarProps,
-  PaymentFormProps,
-} from './components';
-
-export type {
-  DateRange,
-  TimeRange,
-  ValidationResult,
-  PaginatedResponse,
-  QueryParams,
-} from './utils';
+export type { TimeSlot, Student, Rink } from './context';
+export type { ValidationError, PaginationParams } from './utils';
