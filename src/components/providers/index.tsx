d27f491a09d4fe82099840d56664d89a687e0903
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-11 10:05:53 -0500

    Consolidate providers into a single, simple file

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,17 +1,24 @@
 "use client";
 
-import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
+import { ThemeProvider } from 'next-themes';
+import { ReactNode } from 'react';
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
-    <SessionProvider>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
-    </SessionProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+    >
+      <SessionProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
