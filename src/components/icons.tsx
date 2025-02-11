99986967e5dba8c0c1408b0be8f14203782c348d
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-11 13:09:55 -0500

    fix: completely revise icons implementation

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,9 +1,22 @@
-import { Loader2, LucideIcon } from 'lucide-react'
+'use client'
+
+import * as React from 'react'
+import { Loader2 } from 'lucide-react'
 
 export const Icons = {
   spinner: Loader2,
-  google: (props: React.ComponentProps<LucideIcon>) => (
-    <svg {...props} viewBox="0 0 24 24">
+  google: ({ className, ...props }: React.HTMLAttributes<SVGElement>) => (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      data-prefix="fab"
+      data-icon="google"
+      role="img"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className={className}
+      {...props}
+    >
       <path
         d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
         fill="#4285F4"
@@ -22,5 +35,5 @@ export const Icons = {
       />
       <path d="M1 1h22v22H1z" fill="none" />
     </svg>
-  ),
+  )
 }
