da021de9e449bb2ba0d6558e3b9feb73118d0afe
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-10 19:04:21 -0500

    cleanup: remove redundant loading and error components

--- src/components/loading/LoadingPage.tsx
+++
@@ -1,9 +0,0 @@
-import { LoadingSpinner } from './LoadingSpinner';
-
-export function LoadingPage() {
-  return (
-    <div className="flex h-[calc(100vh-4rem)] items-center justify-center">
-      <LoadingSpinner size="lg" />
-    </div>
-  );
-}
