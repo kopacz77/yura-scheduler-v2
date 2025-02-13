c0417c9b35ba93815f64897efafd745cb40e731e
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-13 11:02:52 -0500

    Restore original dashboard page

--- src/app/(protected)/admin/dashboard/page.tsx
+++ src/app/(protected)/admin/dashboard/page.tsx
@@ -2,6 +2,6 @@
 
 import { AdminDashboard } from '@/components/admin/AdminDashboard';
 
-export default function AdminDashboardPage() {
+export default function DashboardPage() {
   return <AdminDashboard />;
 }
