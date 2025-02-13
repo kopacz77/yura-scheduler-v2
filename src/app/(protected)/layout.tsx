dde658afd3f6881157476c4708328e8972bd124c
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-13 11:02:45 -0500

    Revert layout changes to fix build error

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -6,19 +6,9 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen">
-      {/* Sidebar */}
-      <aside className="w-64 border-r bg-white">
-        {/* Sidebar content rendered here */}
-      </aside>
-      
-      {/* Main content */}
-      <div className="flex-1 flex flex-col min-h-0">
-        <main className="flex-1 p-4 overflow-auto">
-          {children}
-        </main>
-        <SiteFooter />
-      </div>
+    <div className="relative flex min-h-screen flex-col">
+      <main className="flex-1 container py-6">{children}</main>
+      <SiteFooter />
     </div>
   );
 }
