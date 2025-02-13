a3df898d3458c5676128262658a54180c41a95bc
kopacz77 <94183077+kopacz77@users.noreply.github.com>
2025-02-13 15:10:11 -0500

    Remove extra spacing from protected layout

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -6,8 +6,8 @@ export default function ProtectedLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex min-h-screen flex-col">
-      <main className="flex-1 container py-6">{children}</main>
+    <div className="flex h-screen">
+      <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
   );
